87fe86e2b9da5ce1b5a6a6c0e318ee68293fef2c
Takanori Ikegawa <ikeo.hogehoge@gmail.com>
2025-01-27 14:18:12 +0900

    feat: 🎸 jsonファイルを編集するときに二重引用符が表示されるようにする

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,7 +3,15 @@ require("config.lazy")
 
 -- ノーマルモードに戻るときに英語入力に切り替える
 vim.api.nvim_create_autocmd("InsertLeave", {
-  callback = function()
-    vim.fn.system("macism com.apple.keylayout.ABC")
-  end,
+	callback = function()
+		vim.fn.system("macism com.apple.keylayout.ABC")
+	end,
+})
+
+-- 二重引用符を表示する
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "json", "jsonc" },
+	callback = function()
+		vim.wo.conceallevel = 0
+	end,
 })
